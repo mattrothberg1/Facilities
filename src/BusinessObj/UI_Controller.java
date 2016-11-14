cd183b534beb274db4343ce4f9e3f06999e81125
mattrothberg1 <mdr218@lehigh.edu>
2016-11-14 12:56:08 -0500

    Added UI controller class
    We will need to update the diagram with a UI controller - def a must
    have

---
+++ src/BusinessObj/UI_Controller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessObj;
+
+/**
+ *
+ * @author mattrothberg
+ */
+public class UI_Controller {
+    public static Boolean checkLogin(String username, String password){
+        //check for the username by logging into the database
+    }
+    
+    
+    
+}
